296cfbe164c43e05a4d05c9fef4a5fa075a14f75
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-09-24 23:17:30 +0600

    project setup complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Pure Django API
+
+1. Create (POST)
+2. Retrieve (GET)
+3. Update (PUT)
+4. Delete (DELETE)
---
+++ updates/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from updates.models import Update
+
+# Register your models here.
+admin.site.register(Update)
